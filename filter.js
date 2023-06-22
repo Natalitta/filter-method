1f75b3ee73cc0e262ac867229abe3e9f345a6e58
Natalitta <nataliialebedieva@gmail.com>
2023-06-22 22:21:12 +0000

    Add filter challenge

--- filter.js
+++ filter.js
@@ -20,6 +20,15 @@ const people = [
     age: 18,
   },
 ];
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === "Paul"); //const paul = people.filter(p => p.name === "Paul"); also valid
+
+const lianna = people.filter(p => p.name === "Lianna")[0]; //to get only an object
+ 
+console.log(paul);
+console.log(lianna)
 
 
 // Complex Filtering
@@ -55,3 +64,61 @@ const students = [
     ]
   },
 ];
+
+const candidates = students.filter(student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+  return strongSkills.length > 0;
+})
+console.log(candidates);
+
+// or
+
+const hasStrongSkills = student => {
+  let strSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+  return strSkills.length > 0;
+};
+const cands = students.filter(hasStrongSkills);
+console.log(cands);
+
+// or 
+const has5yrsExp = skill => skill.yrsExperience >= 5;
+const hasStrSkills = student => student.skills.filter(has5yrsExp).length > 0;
+
+const candids = students.filter(hasStrSkills);
+console.log(candids);
+
+const candsNames = candids.map(candid => candid.name) // to get only names
+console.log(candsNames);
+
+/*Filter challenge*/
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+
+const topMaths = students.filter(itm => itm.results.maths >= 90);
+console.log(topMaths);
